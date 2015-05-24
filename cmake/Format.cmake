3c2b31e429393bf26740d2ff86a5f102bc3af4b0
Alex <robotbrain@robotbrain.info>
2015-05-24 11:53:59 -0400

    NOISSUE Dont add target if no clang-format

--- cmake/Format.cmake
+++ cmake/Format.cmake
@@ -5,23 +5,24 @@ set(FILES_TO_FORMAT)
 add_custom_target(format)
 
 function(Format NAME)
+    if(EXISTS ${CLANGFORMAT_PATH})
+        add_custom_target(format-${NAME})
 
-    add_custom_target(format-${NAME})
-
-    foreach(infile ${ARGN})
-        get_filename_component(dir ${infile} DIRECTORY)
-        if(NOT "${dir}" STREQUAL "${PROJECT_BINARY_DIR}")
-            set(FILES_TO_FORMAT ${FILES_TO_FORMAT} ${infile})
-        endif()
-    endforeach()
+        foreach(infile ${ARGN})
+            get_filename_component(dir ${infile} DIRECTORY)
+            if(NOT "${dir}" STREQUAL "${PROJECT_BINARY_DIR}")
+                set(FILES_TO_FORMAT ${FILES_TO_FORMAT} ${infile})
+            endif()
+        endforeach()
 
 
-    foreach(file ${FILES_TO_FORMAT})
-        add_custom_command(TARGET format-${NAME}
-        COMMAND echo Processing ${file}
-        COMMAND ${CLANGFORMAT_PATH} --style=file -i ${file}
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-    endforeach()
+        foreach(file ${FILES_TO_FORMAT})
+            add_custom_command(TARGET format-${NAME}
+            COMMAND echo Processing ${file}
+            COMMAND ${CLANGFORMAT_PATH} --style=file -i ${file}
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+        endforeach()
+    endif()
     
     add_dependencies(format format-${NAME})
 
