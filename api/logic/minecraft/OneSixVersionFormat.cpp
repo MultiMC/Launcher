aa068688932b0278d274e91da0618c159ed72219
GGWarriorWP <trashichttt@gmail.com>
2020-03-26 10:53:13 +0000

    Remove forgewrapper blocking
    This is just childish, nobody expects you to officially support it, just don't block people from using their favorite launcher working with forge.

--- api/logic/minecraft/OneSixVersionFormat.cpp
+++ api/logic/minecraft/OneSixVersionFormat.cpp
@@ -151,10 +151,6 @@ VersionFilePtr OneSixVersionFormat::versionFileFromJson(const QJsonDocument &doc
             QJsonObject libObj = requireObject(libVal);
             // parse the library
             auto lib = libraryFromJson(libObj, filename);
-            if(lib->rawName().artifactId() == "ForgeWrapper") {
-                out->mainClass.clear();
-                out->addProblem(ProblemSeverity::Error, QObject::tr("Forge workarounds have no place in MultiMC."));
-            }
             out->libraries.append(lib);
         }
     };
