80b0cc620c528d0707aa85406795733c3631b235
Florian Schüller <florian.schueller@gmail.com>
2020-04-13 18:58:41 +0200

    Add support for native minecraft saves
    also removing debug output

--- application/package/ubuntu/multimc/opt/multimc/multimc_copy_instance.sh
+++ application/package/ubuntu/multimc/opt/multimc/multimc_copy_instance.sh
@@ -18,9 +18,18 @@ DST=$2
 [ -z $DST ] && DST=localhost
 echo "Destination: $DST"
 
-M_PATH=.local/share/multimc/instances/
+echo "----"
+echo "Select to work on MulitMC instances or worlds of 'native' minecraft"
+select M_PATH in .local/share/multimc/instances/ .minecraft/saves/; do
+	if [ -z $M_PATH ]; then
+                echo "ERROR: No selection"
+                exit 1
+        fi
+        break
+done
 
-echo "Getting MultiMC instances of $SRC"
+
+echo "Getting list from $SRC"
 LIST=( "$(ssh $SRC find $M_PATH -maxdepth 1 -mindepth 1 -type d -printf \"$SRC:%p\\n\" |grep -v _MMC_TEMP)" )
 
 IFS=$'\n'
@@ -35,10 +44,10 @@ done
 
 TMP_DIR=$(mktemp -d)
 scp -r "$SRC_PATH" $TMP_DIR
-find $TMP_DIR
 ssh $DST mkdir -p $M_PATH
 NAME=$(ls $TMP_DIR)
 
-read -p "WARNING $NAME will be deleted on $DST first, if it exists! (<RETURN> to continue, CTRL-C to cancel)"
+echo ""
+read -p "WARNING '$NAME' will be deleted on $DST first, if it exists! (<RETURN> to continue, CTRL-C to cancel)"
 ssh $DST rm -rf $M_PATH/${NAME// /\\ }
 scp -r $TMP_DIR/* "$DST:$M_PATH"
