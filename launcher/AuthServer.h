f131f4a1ebf10a1f03a3569749fc9a600166bd1a
Sebastian <sebastianmontoya209@gmail.com>
2021-09-13 08:17:17 -0500

    Use logic export

--- launcher/AuthServer.h
+++ launcher/AuthServer.h
@@ -3,9 +3,9 @@
 #include <QString>
 #include <QTcpServer>
 #include "settings/SettingsObject.h"
-#include "logic_export.h"
+#include "multimc_logic_export.h"
 
-class LOGIC_EXPORT AuthServer: public QObject
+class MULTIMC_LOGIC_EXPORT AuthServer: public QObject
 {
 public:
     explicit AuthServer(QObject *parent = 0);
