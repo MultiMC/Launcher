9f3c2921f24ec453a870733e0b84d8bc1122f4a5
Petr Mrázek <peterix@gmail.com>
2023-06-21 09:47:49 +0200

    Remove unnecessary Qt version check
    We are on Qt 6 and not looking back

--- launcher/ui/pages/global/ExternalToolsPage.cpp
+++ launcher/ui/pages/global/ExternalToolsPage.cpp
@@ -33,11 +33,7 @@ ExternalToolsPage::ExternalToolsPage(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->tabWidget->tabBar()->hide();
-
-    #if QT_VERSION >= QT_VERSION_CHECK(5, 2, 0)
     ui->jsonEditorTextBox->setClearButtonEnabled(true);
-    #endif
-
     ui->mceditLink->setOpenExternalLinks(true);
     ui->jvisualvmLink->setOpenExternalLinks(true);
     ui->jprofilerLink->setOpenExternalLinks(true);
