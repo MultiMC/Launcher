e8400219fa84a3295d7997974c423b11538858b9
Petr Mrázek <peterix@gmail.com>
2025-01-26 15:13:59 +0100

    NOISSUE fill in missing copyright notices

--- launcher/mojang/PackageInstallTask.h
+++ launcher/mojang/PackageInstallTask.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2023 Petr Mrázek
+ *
+ * This source is subject to the Microsoft Permissive License (MS-PL).
+ * Please see the COPYING.md file for more information.
+ */
+
 #pragma once
 
 #include "tasks/Task.h"
